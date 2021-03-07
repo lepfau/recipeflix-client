b5e114e343df356c9da87822c59e7c1ff51591b4
Edouard Pfauwadel <edouard.pfauwadel@gmail.com>
2021-03-07 17:56:47 +0100

    test

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,18 @@
 import React from "react";
-import FormCreate from "../components/Forms/FormCreate"
-
+import FormCreate from "../components/Forms/FormCreate";
 
 class Home extends React.Component {
   render() {
     return (
       <div>
-        <h1>Welcome to Recipee App! ∆ <img alt="vegetables" src="https://wallpaperaccess.com/full/1306571.jpg"/></h1>
-        <FormCreate/>
+        <h2>
+          Welcome to Recipee App! ∆{" "}
+          <img
+            alt="vegetables"
+            src="https://wallpaperaccess.com/full/1306571.jpg"
+          />
+        </h2>
+        <FormCreate />
       </div>
     );
   }
